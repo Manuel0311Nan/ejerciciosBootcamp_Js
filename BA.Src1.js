b5380ea52838158457d35eec9e547d1ddd3bf06f
Manuel0311Nan <manuzz3112@gmail.com>
2022-03-13 19:36:10 +0100

    Parte 1 ejercicios JS

---
+++ BA.Src1.js
@@ -0,0 +1,17 @@
+/* Declaración de variables */
+
+let myFavouriteHero = "Hulk";
+
+let x = 50;
+
+let h = 5;
+
+let y = 10;
+
+let z = h + y;
+
+console.log(myFavouriteHero);
+console.log(x);
+console.log(h);
+console.log(y);
+console.log(z);
